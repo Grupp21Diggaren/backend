1766b14b53790647f7c7d2ed43bb14177c572558
edinj777 <128481975+edinj777@users.noreply.github.com>
2024-01-08 17:12:09 +0100

    a

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import Entity.WikiAPI;
 
@@ -19,9 +21,15 @@ public class Controller {
     }
 
     @GetMapping("/api/biography/{artistName}")
-    public String getBiography(@PathVariable("artistName") String artistName) {
+    public ResponseEntity<String> getBiography(@PathVariable("artistName") String artistName) {
         System.out.println(WikiAPI.getBiographyFromWikiAPI(artistName));
-        return "Biography for " + artistName + ": " + WikiAPI.getBiographyFromWikiAPI(artistName);
+        String bio = WikiAPI.getBiographyFromWikiAPI(artistName);
+        if (bio != null) {
+            return ResponseEntity.ok(bio);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Biography not found for " + artistName);
+        }
+        //return "Biography for " + artistName + ": " + WikiAPI.getBiographyFromWikiAPI(artistName);
     }
 
 

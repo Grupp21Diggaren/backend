54b53bbe20cfffd8a2f1737eacbe79fe1eb89424
edinj777 <128481975+edinj777@users.noreply.github.com>
2024-01-08 15:47:15 +0100

    cors

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,11 +1,11 @@
 package Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import Entity.WikiAPI;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "127.0.0.1:5500")
 public class Controller {
     private WikiAPI wikiAPI = new WikiAPI();
 
